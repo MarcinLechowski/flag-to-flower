bc53f1931effc614c96693a7fff330faa9f70229
MarcinLechowski <marcin.lechowski2@gmail.com>
2024-02-08 18:05:53 +0100

    próba

--- src/main/java/com/Kaer/flagtoflower/flower/Flower.java
+++ src/main/java/com/Kaer/flagtoflower/flower/Flower.java
@@ -1,8 +1,9 @@
 package com.Kaer.flagtoflower.flower;
 
 import lombok.Builder;
-import lombok.Data;
+import lombok.Data;//
 
+//&&&&&&&&&&&&&&&&&PROBA COMMITA
 @Data
 @Builder//Dzięki temu, można łatwo tworzyć obiekty z różnymi zestawami pól, a kod staje się bardziej czytelny.
 public class Flower {
